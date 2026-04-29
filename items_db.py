e718d3f818b66ca37e64d6e51350983936225cc0
UP230823 <up230823@alumnos.upa.edu.mx>
2026-04-28 22:03:40 -0600

    Toda la verdura

--- items_db.py
+++ items_db.py
@@ -42,14 +42,54 @@ CATEGORIAS = {
     "Árbol: Arcanos": {
         "MAIN_ARCANESTAFF": "Bastón arcano (1H)", "2H_ARCANESTAFF": "Gran bastón arcano", "2H_ENIGMATICSTAFF": "Bastón enigmático", "MAIN_ARCANESTAFF_MORGANA": "Bastón bruja", "2H_ARCANESTAFF_HELL": "Bastón oculto", "2H_ARCANESTAFF_RUNIC_RED": "Bastón vacío", "2H_ARCANESTAFF_AVALON": "Bastón energía", "2H_ARCANESTAFF_CRYSTAL": "Arcano cristal"
     },
-    "Pecheras (Armor N=192)": {
-        "ARMOR_PLATE_SET1": "Pechera Soldado", "ARMOR_PLATE_SET2": "Pechera Guardián", "ARMOR_PLATE_SET3": "Pechera Caballero", "ARMOR_LEATHER_SET1": "Chaqueta Mercenario", "ARMOR_LEATHER_SET2": "Chaqueta Cazador", "ARMOR_LEATHER_SET3": "Chaqueta Asesino", "ARMOR_CLOTH_SET1": "Túnica Erudito", "ARMOR_CLOTH_SET2": "Túnica Clérigo", "ARMOR_CLOTH_SET3": "Túnica Mago"
+   "Pecheras: Placas (N=192)": {
+        "ARMOR_PLATE_SET1": "Pechera Soldado", "ARMOR_PLATE_SET2": "Pechera Caballero", "ARMOR_PLATE_SET3": "Pechera Guardián",
+        "ARMOR_PLATE_KEEPER": "Pechera Sepulturero", "ARMOR_PLATE_HELL": "Pechera Demonio", "ARMOR_PLATE_UNDEAD": "Pechera Juez",
+        "ARMOR_PLATE_AVALON": "Pechera de Valor", "ARMOR_PLATE_ROYAL": "Pechera Real"
     },
-    "Cascos y Botas (N=96)": {
-        "HEAD_PLATE_SET1": "Casco Soldado", "HEAD_PLATE_SET2": "Casco Guardián", "HEAD_PLATE_SET3": "Casco Caballero", "HEAD_LEATHER_SET1": "Capucha Mercenario", "HEAD_LEATHER_SET2": "Capucha Cazador", "HEAD_LEATHER_SET3": "Capucha Asesino", "HEAD_CLOTH_SET1": "Caperuza Erudito", "HEAD_CLOTH_SET2": "Caperuza Clérigo", "HEAD_CLOTH_SET3": "Caperuza Mago",
-        "SHOES_PLATE_SET1": "Botas Soldado", "SHOES_PLATE_SET2": "Botas Guardián", "SHOES_PLATE_SET3": "Botas Caballero", "SHOES_LEATHER_SET1": "Botas Mercenario", "SHOES_LEATHER_SET2": "Botas Cazador", "SHOES_LEATHER_SET3": "Botas Asesino", "SHOES_CLOTH_SET1": "Sandalias Erudito", "SHOES_CLOTH_SET2": "Sandalias Clérigo", "SHOES_CLOTH_SET3": "Sandalias Mago"
+    "Pecheras: Cuero (N=192)": {
+        "ARMOR_LEATHER_SET1": "Chaqueta Mercenario", "ARMOR_LEATHER_SET2": "Chaqueta Cazador", "ARMOR_LEATHER_SET3": "Chaqueta Asesino",
+        "ARMOR_LEATHER_MORGANA": "Chaqueta Acechador", "ARMOR_LEATHER_HELL": "Chaqueta Endemoniado", "ARMOR_LEATHER_UNDEAD": "Chaqueta Espectro",
+        "ARMOR_LEATHER_AVALON": "Chaqueta Tenacidad", "ARMOR_LEATHER_ROYAL": "Chaqueta Real"
     },
-    "Accesorios y Off-hands (N=96)": {
-        "BAG": "Bolsa", "CAPE": "Capa", "SHIELD": "Escudo", "TORCH": "Antorcha", "TOME": "Libro de hechizos"
+    "Pecheras: Tela (N=192)": {
+        "ARMOR_CLOTH_SET1": "Túnica Erudito", "ARMOR_CLOTH_SET2": "Túnica Clérigo", "ARMOR_CLOTH_SET3": "Túnica Mago",
+        "ARMOR_CLOTH_KEEPER": "Túnica Druida", "ARMOR_CLOTH_HELL": "Túnica Maligno", "ARMOR_CLOTH_MORGANA": "Túnica Cultista",
+        "ARMOR_CLOTH_AVALON": "Túnica Pureza", "ARMOR_CLOTH_ROYAL": "Túnica Real"
+    },
+    "Cascos: Todos los tipos (N=96)": {
+        "HEAD_PLATE_SET1": "Casco Soldado", "HEAD_PLATE_KEEPER": "Casco Sepulturero", "HEAD_PLATE_HELL": "Casco Demonio", "HEAD_PLATE_UNDEAD": "Casco Juez", "HEAD_PLATE_AVALON": "Casco Valor", "HEAD_PLATE_ROYAL": "Casco Real",
+        "HEAD_LEATHER_SET1": "Capucha Mercenario", "HEAD_LEATHER_MORGANA": "Capucha Acechador", "HEAD_LEATHER_HELL": "Capucha Endemoniado", "HEAD_LEATHER_UNDEAD": "Capucha Espectro", "HEAD_LEATHER_AVALON": "Capucha Tenacidad", "HEAD_LEATHER_ROYAL": "Capucha Real",
+        "HEAD_CLOTH_SET1": "Caperuza Erudito", "HEAD_CLOTH_KEEPER": "Caperuza Druida", "HEAD_CLOTH_HELL": "Caperuza Maligno", "HEAD_CLOTH_MORGANA": "Caperuza Cultista", "HEAD_CLOTH_AVALON": "Caperuza Pureza", "HEAD_CLOTH_ROYAL": "Caperuza Real"
+    },
+    "Botas: Todos los tipos (N=96)": {
+        "SHOES_PLATE_SET1": "Botas Soldado", "SHOES_PLATE_KEEPER": "Botas Sepulturero", "SHOES_PLATE_HELL": "Botas Demonio", "SHOES_PLATE_UNDEAD": "Botas Juez", "SHOES_PLATE_AVALON": "Botas Valor", "SHOES_PLATE_ROYAL": "Botas Reales",
+        "SHOES_LEATHER_SET1": "Botas Mercenario", "SHOES_LEATHER_MORGANA": "Botas Acechador", "SHOES_LEATHER_HELL": "Botas Endemoniado", "SHOES_LEATHER_UNDEAD": "Botas Espectro", "SHOES_LEATHER_AVALON": "Botas Tenacidad", "SHOES_LEATHER_ROYAL": "Zapatos Reales",
+        "SHOES_CLOTH_SET1": "Sandalias Erudito", "SHOES_CLOTH_KEEPER": "Sandalias Druida", "SHOES_CLOTH_HELL": "Sandalias Maligno", "SHOES_CLOTH_MORGANA": "Sandalias Cultista", "SHOES_CLOTH_AVALON": "Sandalias Pureza", "SHOES_CLOTH_ROYAL": "Sandalias Reales"
+    },
+    "Bolsas (N=192)": {
+        "BAG": "Bolsa Normal", "BAG_INSIGHT": "Bolsa de Visión"
+    },
+    "Capas (N=96)": {
+        "CAPE": "Capa Normal", 
+        "CAPEITEM_FW_BRIDGEWATCH": "Capa Bridgewatch", "CAPEITEM_FW_FORTSTERLING": "Capa Fort Sterling",
+        "CAPEITEM_FW_LYMHURST": "Capa Lymhurst", "CAPEITEM_FW_MARTLOCK": "Capa Martlock",
+        "CAPEITEM_FW_THETFORD": "Capa Thetford", "CAPEITEM_FW_CAERLEON": "Capa Caerleon",
+        "CAPEITEM_FW_BRECILIEN": "Capa Brecilien",
+        "CAPEITEM_DEMON": "Capa Demonio", "CAPEITEM_UNDEAD": "Capa Muerto Vivo",
+        "CAPEITEM_KEEPER": "Capa Hereje (Keeper)", "CAPEITEM_MORGANA": "Capa Morgana",
+        "CAPEITEM_HERETIC": "Capa Hereje", "CAPEITEM_AVALON": "Capa Avalónica"
+    },
+    "Off-hands: Escudos (N=96)": {
+        "SHIELD": "Escudo Normal", "SHIELD_HELL": "Sarcófago", "SHIELD_KEEPER": "Escudo Caitiff",
+        "SHIELD_MORGANA": "Rompecaras", "SHIELD_AVALON": "Égida Astral"
+    },
+    "Off-hands: Antorchas (N=96)": {
+        "TORCH": "Antorcha Normal", "TORCH_HELL": "Invocanieblas", "TORCH_KEEPER": "Vela de Cripta",
+        "TORCH_MORGANA": "Bastón Burlón", "TORCH_AVALON": "Cetro Sagrado"
+    },
+    "Off-hands: Libros (N=96)": {
+        "TOME": "Libro de Hechizos", "TOME_HELL": "Muisak", "TOME_KEEPER": "Raíz Primordial",
+        "TOME_MORGANA": "Ojo de los Secretos", "TOME_AVALON": "Incensario Celestial"
     }
 }
